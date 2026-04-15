ef050d4dc72689182a47424a867b5178c263a64e
Angi-Adema <dr.adema@gmail.com>
2026-04-14 20:12:33 -0600

    created the method prductNum() passing in the int array of the user's input of five numbers as well as the index of the array. added the base case of the length of the array and returning 1 as we are working with the product. Returning the index value times the product of the remaining elements after the current index. In main() created the Scanner object and prompted the user to enter 5 numbers. The five numbers are store in an array, we loop through the responses of the user to store the number in the array and store this result by recursively calling the productNum() method passing in the array and the index starting at 0.

--- src/com/angiadema/recursion/RecursionExample.java
+++ src/com/angiadema/recursion/RecursionExample.java
@@ -1,9 +1,44 @@
 package com.angiadema.recursion;
 
+import java.util.Scanner;
+
 public class RecursionExample {
+	
+	// Recursive method
+	public static int productNum(int[] numArray, int arrIndex) {
+		
+		// Base case
+		if (arrIndex == numArray.length) {
+			return 1;
+		}
+		
+		// Recursive logic taking the value of the current index and multiplying
+		// it by the product of all the remaining elements after this index.
+		return numArray[arrIndex] * productNum(numArray, arrIndex + 1);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		// Initialize a new Scanner object for user input
+		Scanner scnr = new Scanner(System.in);
+		
+		// Variable holding the product of input numbers
+		int result;
+		
+		// Array to store user input of 5 numbers
+		int[] numArray = new int[5];
+		
+		// Prompt user to enter five numbers
+		System.out.println("Please enter five numbers: ");
+		
+		// Loop through the input numbers and store them in the array
+		for (int i = 0; i < 5; i++) {
+			numArray[i] = scnr.nextInt();
+		}
+		
+		// Multiply all the numbers in the array starting at index 0
+		result = productNum(numArray, 0);
+		
+
 
 	}
 
